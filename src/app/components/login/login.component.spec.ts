00a2c770dd393c16450741d27d8f60013869b6b4
spiros3p <spiros3p@gmail.com>
2023-11-07 23:45:20 +0100

    f

--- src/app/components/login/login.component.spec.ts
+++ src/app/components/login/login.component.spec.ts
@@ -3,12 +3,12 @@ import { Spectator, createComponentFactory } from '@ngneat/spectator';
 import { LoginComponent } from './login.component';
 
 describe('LoginComponent', () => {
-  let spectator: Spectator<LoginComponent>;
-  const createComponent = createComponentFactory(LoginComponent);
+    let spectator: Spectator<LoginComponent>;
+    const createComponent = createComponentFactory(LoginComponent);
 
-  it('should create', () => {
-    spectator = createComponent();
+    it('should create', () => {
+        spectator = createComponent();
 
-    expect(spectator.component).toBeTruthy();
-  });
+        expect(spectator.component).toBeTruthy();
+    });
 });
