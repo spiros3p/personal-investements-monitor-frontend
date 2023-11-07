924ed1a2d037a8508c6b55bdc166fcf6c6d214bd
spiros3p <spiros3p@gmail.com>
2023-11-07 23:39:26 +0100

    add auth.guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PageAllChartsComponent } from './components/page-all-charts/page-all-charts.component';
+import { AuthGuard } from 'src/app/guards/auth.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: '', component: PageAllChartsComponent, canActivate: [AuthGuard] },
+    // { path: 'login', component: LoginComponent },
+];
 
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
