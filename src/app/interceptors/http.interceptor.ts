97d9d1cb7fdfd0b7b55673080df34f74c88fece0
spiros3p <spiros3p@gmail.com>
2023-11-09 01:38:36 +0100

    desp

--- src/app/interceptors/http.interceptor.ts
+++ src/app/interceptors/http.interceptor.ts
@@ -6,22 +6,32 @@ import {
     HttpRequest,
 } from '@angular/common/http';
 
-import { Observable } from 'rxjs';
+import { Observable, tap } from 'rxjs';
 
 /** Inject With Credentials into the request */
 @Injectable()
 export class HttpRequestInterceptor implements HttpInterceptor {
-    /**
-     * HTTP interceptor that sets Angular to include cookies with each outgoing request.
-     * By defualt Angular does not allow that due to CORS and security settings
-     * @param req
-     * @param next
-     * @returns
-     */
+    m: any;
+
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        req = req.clone({
-            withCredentials: true,
-        });
-        return next.handle(req);
+        if (this.m) {
+            req = req.clone({
+                withCredentials: true,
+                headers: req.headers.set('Cookie', this.m),
+            });
+        } else {
+            req = req.clone({
+                withCredentials: true,
+            });
+        }
+
+        return next.handle(req).pipe(
+            tap((e) => {
+                if ((e as any)?.body?.cook) {
+                    this.m = (e as any).body.cook;
+                    console.log(this.m);
+                }
+            })
+        );
     }
 }
