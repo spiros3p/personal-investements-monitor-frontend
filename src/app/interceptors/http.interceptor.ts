2a6e49d76c00b6042674f5195a1adcccc0c416c6
spiros3p <spiros3p@gmail.com>
2023-11-10 21:54:44 +0100

    interceptor - add Authorization header

--- src/app/interceptors/http.interceptor.ts
+++ src/app/interceptors/http.interceptor.ts
@@ -11,25 +11,17 @@ import { Observable, tap } from 'rxjs';
 /** Inject With Credentials into the request */
 @Injectable()
 export class HttpRequestInterceptor implements HttpInterceptor {
-    m: any;
+    token: any;
 
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        if (this.m) {
-            req = req.clone({
-                withCredentials: true,
-                headers: req.headers.set('Cookie', this.m),
-            });
-        } else {
-            req = req.clone({
-                withCredentials: true,
-            });
-        }
+        req = req.clone({
+            headers: req.headers.set('Authorization', `Bearer ${this.token}`),
+        });
 
         return next.handle(req).pipe(
-            tap((e) => {
-                if ((e as any)?.body?.cook) {
-                    this.m = (e as any).body.cook;
-                    console.log(this.m);
+            tap((res: any) => {
+                if (res?.body?.token) {
+                    this.token = res?.body?.token;
                 }
             })
         );
